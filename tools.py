10566b0546bba390f6a3ae4ab75a62a47617ab53
Awais Tanveer <awaistanveer210@gmail.com>
2026-07-16 09:43:54 +0500

    upload new task in pycham

---
+++ tools.py
@@ -0,0 +1,16 @@
+def area_circle(r):
+    return 3.14 * r * r
+
+def area_triangle(b, h):
+    return 0.5 * b * h
+
+def area_square(s):
+    return s * s      # <-- yahan bhi correction ki hai
+
+def area_rectangle(w, h):
+    return w * h
+
+# print("Area of Circle:", area_circle(5))
+# print("Area of Triangle:", area_triangle(10, 8))
+# print("Area of Square:", area_square(6))
+# print("Area of Rectangle:", area_rectangle(8, 5))
